10e113d93d16bcf41aad8c318f1d65ceee4e8dd9
vulto <jhsoperacao@gmail.com>
2023-09-21 22:36:20 -0300

    Created build folder and moved build result to there.

--- nobuild.c
+++ nobuild.c
@@ -1,69 +1,71 @@
 #define NOBUILD_IMPLEMENTATION
 #include "./nobuild.h"
 
-#define BIN "smw"
+#define BIN "build/smw"
 #define DEBUGGER "gf2"
 #define DESTDIR "/usr/local/bin"
 #define CACHE "assets/__pycache__"
+#define CC "clang"
+
 //				"-Wextra", \
 //				"-Werror", \
 //				"-Wfatal-errors", \
 //				"-pedantic", \
 //				"-pedantic-errors", \
+//				"-Wall", \
+//				"-O0",
+//				"-std=c2x", \
 
-#define CFLAGS  "-O0", \
-				"-fno-strict-aliasing", \
-				"-Wall", \
+#define CFLAGS  "-fno-strict-aliasing", \
 				"-Wno-unused-variable", \
-				"-std=c2x", \
 				"-I/usr/include/SDL2", \
                 "-DSYSTEM_VOLUME_MIXER_AVAILABLE=0", \
                 "-I."
 
-#define SRCS  "smb1/smb1_00.c", \
-              "smb1/smb1_01.c", \
-              "smb1/smb1_02.c", \
-              "smb1/smb1_08.c", \
-              "smb1/smb1_cpu_infra.c", \
-              "smb1/smb1_spc_player.c", \
-              "smbll/smbll_00.c", \
-              "smbll/smbll_01.c", \
-              "smbll/smbll_02.c", \
-              "smbll/smbll_08.c", \
-              "smbll/smbll_0b.c", \
-              "smbll/smbll_cpu_infra.c", \
-              "src/common_cpu_infra.c", \
-              "src/common_rtl.c", \
-              "src/config.c", \
-              "src/glsl_shader.c", \
-              "src/lm.c", \
-              "src/main.c", \
-              "src/opengl.c", \
-              "src/smw_00.c", \
-              "src/smw_01.c", \
-              "src/smw_02.c", \
-              "src/smw_03.c", \
-              "src/smw_04.c", \
-              "src/smw_05.c", \
-              "src/smw_07.c", \
-              "src/smw_0c.c", \
-              "src/smw_0d.c", \
-              "src/smw_cpu_infra.c", \
-              "src/smw_rtl.c", \
-              "src/smw_spc_player.c", \
-              "src/tracing.c", \
-              "src/util.c", \
-              "src/snes/apu.c", \
-              "src/snes/cart.c", \
-              "src/snes/cpu.c", \
-              "src/snes/dma.c", \
-              "src/snes/dsp.c", \
-              "src/snes/ppu.c", \
-              "src/snes/ppu_old.c", \
-              "src/snes/snes.c", \
-              "src/snes/snes_other.c", \
-              "src/snes/spc.c", \
-              "third_party/gl_core/gl_core_3_1.c"
+#define SRCS "smb1/smb1_00.c", \
+             "smb1/smb1_01.c", \
+             "smb1/smb1_02.c", \
+             "smb1/smb1_08.c", \
+             "smb1/smb1_cpu_infra.c", \
+             "smb1/smb1_spc_player.c", \
+             "smbll/smbll_00.c", \
+             "smbll/smbll_01.c", \
+             "smbll/smbll_02.c", \
+             "smbll/smbll_08.c", \
+             "smbll/smbll_0b.c", \
+             "smbll/smbll_cpu_infra.c", \
+             "src/common_cpu_infra.c", \
+             "src/common_rtl.c", \
+             "src/config.c", \
+             "src/glsl_shader.c", \
+             "src/lm.c", \
+             "src/main.c", \
+             "src/opengl.c", \
+             "src/smw_00.c", \
+             "src/smw_01.c", \
+             "src/smw_02.c", \
+             "src/smw_03.c", \
+             "src/smw_04.c", \
+             "src/smw_05.c", \
+             "src/smw_07.c", \
+             "src/smw_0c.c", \
+             "src/smw_0d.c", \
+             "src/smw_cpu_infra.c", \
+             "src/smw_rtl.c", \
+             "src/smw_spc_player.c", \
+             "src/tracing.c", \
+             "src/util.c", \
+             "src/snes/apu.c", \
+             "src/snes/cart.c", \
+             "src/snes/cpu.c", \
+             "src/snes/dma.c", \
+             "src/snes/dsp.c", \
+             "src/snes/ppu.c", \
+             "src/snes/ppu_old.c", \
+             "src/snes/snes.c", \
+             "src/snes/snes_other.c", \
+             "src/snes/spc.c", \
+             "third_party/gl_core/gl_core_3_1.c"
 
 /* TODO:
  *		 buld objs separately
@@ -72,14 +74,9 @@
  *		 enable extra warnings
  */
 
-const char *cc(void){
-    const char *result = getenv("CC");
-    return result ? result : "cc";
-}
-
 int posix_main(int argc, char **argv){
     if (argc == 1){
-    	CMD(cc(), "-o", BIN, SRCS, CFLAGS, "-lm", "-lSDL2", NULL);
+    	CMD(CC, "-o", BIN, SRCS, CFLAGS, "-lm", "-lSDL2", NULL);
     	CMD("python3", "assets/restool.py", NULL);
 	}
 
